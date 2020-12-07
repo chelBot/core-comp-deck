4a807def5ed3b0ece5d8290bfe88950fab6cb3cd
Chelsea Stauber <Chelsea.Stauber@resmed.com>
2020-12-07 11:27:01 -0800

    Implement Bonnie's changes.

--- es2015.js
+++ es2015.js
@@ -114,7 +114,6 @@
     var rating = e.target.id || e.target.parentNode.id;
     console.log(e.target, rating);
     values[id][1] = rating;
-    console.log(values);
 
     if (rating === "positive") {
       neutral.classList.remove("selected");
@@ -215,39 +214,51 @@
 
     if (currentSlide === slides.length - 1) {
       nextBtn.style.visibility = "hidden";
-      console.log(nextBtn.firstChild.src);
       previousBtn.style.visibility = "hidden";
       previousBtn.firstChild.src = "assets/restart.svg";
       footer.style.visibility = "hidden";
-      modal.style.backgroundColor = "#fff";
-      close.style.color = "#696969";
-      close.classList.add("resultsHover");
+      // modal.style.backgroundColor = "#fff";
+      // close.style.color = "#696969";
+      // close.classList.add("resultsHover");
       populateArrays();
       populateLists();
     } else {
       nextBtn.style.visibility = "visible";
       footer.style.visibility = "visible";
-      close.style.color = "#fff";
-      close.classList.remove("resultsHover");
+      // close.style.color = "#fff";
+      // close.classList.remove("resultsHover");
       previousBtn.firstElementChild.src = "assets/previous.svg";
-      previousBtn.style.color = "#fff";
+      // previousBtn.style.color = "#fff";
     }
 
     if (currentSlide >= 0 && currentSlide <= 3) {
-      modal.style.backgroundColor = "#0077c0";
+      // modal.style.backgroundColor = "#0077c0";
+      positive.style.backgroundColor = "#0077c0";
+      neutral.style.backgroundColor = "#0077c0";
+      negative.style.backgroundColor = "#0077c0";
     } else if (currentSlide > 3 && currentSlide <= 7) {
-      modal.style.backgroundColor = "#4156a6";
+      // modal.style.backgroundColor = "#4156a6";
+      positive.style.backgroundColor = "#4156a6";
+      neutral.style.backgroundColor = "#4156a6";
+      negative.style.backgroundColor = "#4156a6";
     } else if (currentSlide > 7 && currentSlide <= 11) {
-      modal.style.backgroundColor = "#6f2c91";
+      // modal.style.backgroundColor = "#6f2c91";
+      positive.style.backgroundColor = "#6f2c91";
+      neutral.style.backgroundColor = "#6f2c91";
+      negative.style.backgroundColor = "#6f2c91";
     } else if (currentSlide > 11 && currentSlide <= 15) {
-      modal.style.backgroundColor = "#b72755";
+      // modal.style.backgroundColor = "#b72755";
+      positive.style.backgroundColor = "#b72755";
+      neutral.style.backgroundColor = "#b72755";
+      negative.style.backgroundColor = "#b72755";
     } else if (currentSlide >= 15 && currentSlide <= 19) {
-      modal.style.backgroundColor = "#ed1b2f";
+      // modal.style.backgroundColor = "#ed1b2f";
+      positive.style.backgroundColor = "#ed1b2f";
+      neutral.style.backgroundColor = "#ed1b2f";
+      negative.style.backgroundColor = "#ed1b2f";
     } else {
-      modal.style.backgroundColor = "#fff";
+      // modal.style.backgroundColor = "#fff";
     }
-
-    console.log(currentSlide);
   }
 
   function showNextSlide() {
@@ -272,6 +283,9 @@
   previousBtn.addEventListener("click", showPreviousSlide);
   nextBtn.addEventListener("click", showNextSlide);
   restart.addEventListener("click", function () {
+    positiveLIs = [];
+    neturalLIs = [];
+    negativeLIs = [];
     showSlide(0);
   });
 })();
